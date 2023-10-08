74926c06939c05fe8208035be4665fb2b0e589b7
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-10-08 05:24:36 +0000

    setup protected route

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { redirect } from "@/node_modules/next/navigation";
+import Link from "@/node_modules/next/link";
+
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    // redirect("/api/auth/signin");
+    return (
+      <>
+        <p>You are not logged in...</p>
+        <Link href={`/api/auth/signin`}>
+          <p>Click here to signup/login</p>
+        </Link>
+      </>
+    );
+  }
   return (
     <main>
       <h1>Hello next</h1>
